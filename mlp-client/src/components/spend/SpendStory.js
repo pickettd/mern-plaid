5fc908d5216df310323187117791f4dad32e885a
David Pickett <pickettd@gmail.com>
2020-07-24 16:30:37 -1000

    Change spend story component to class style in order to re-use mvp state/redux code and add example of displaying list of accounts

--- mlp-client/src/components/spend/SpendStory.js
+++ mlp-client/src/components/spend/SpendStory.js
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import {
+  getTransactions,
+  addAccount,
+  refreshAccount,
+  deleteAccount,
+} from "../../actions/accountActions";
+import { logoutUser } from "../../actions/authActions";
 import { Link } from "react-router-dom";
 
-const SpendStory = () => {
-  return (
-    <>
-      <div>
-        <span>This is the Spend Story</span>
-        <ul>
-          <li>
-            <Link to="/manage-transactions">Manage Transactions</Link>
-          </li>
-          <li>
-            <Link to="/spend-plan">Spend Plan</Link>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+class SpendStory extends Component {
+  render() {
+    const accountItems = this.props.plaid.accounts.map((account) => (
+      <div key={account._id}>Account Name here: {account.institutionName}</div>
+    ));
+    return (
+      <>
+        <div>
+          <span>This is the Spend Story</span>
+          {accountItems}
+          <ul>
+            <li>
+              <Link to="/manage-transactions">Manage Transactions</Link>
+            </li>
+            <li>
+              <Link to="/spend-plan">Spend Plan</Link>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
+
+SpendStory.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  getTransactions: PropTypes.func.isRequired,
+  addAccount: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
+  refreshAccount: PropTypes.func.isRequired,
+  plaid: PropTypes.object.isRequired,
 };
 
-export default SpendStory;
+const mapStateToProps = (state) => ({
+  plaid: state.plaid,
+});
+
+export default connect(mapStateToProps, {
+  logoutUser,
+  getTransactions,
+  addAccount,
+  deleteAccount,
+  refreshAccount,
+})(SpendStory);
