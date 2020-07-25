0254205979debc553c9f917ea7f3d4a22624642b
David Pickett <pickettd@gmail.com>
2020-07-24 21:12:24 -1000

    Update spend plan to display a bootstrap table and fill in rows of the SpendPlanRow component being connected to the state of categories this month

--- mlp-client/src/components/spend/SpendPlan.js
+++ mlp-client/src/components/spend/SpendPlan.js
@@ -1,11 +1,34 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
+import { connect } from "react-redux";
+import SpendPlanRow from "./SpendPlanRow";
 
-const SpendPlan = () => {
+const SpendPlan = (props) => {
   return (
     <>
       <div>This is the Spend Plan</div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Bank category name</th>
+            <th>My category name</th>
+            <th>Budget for 30 days</th>
+            <th>Spent last 30 days</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.categoriesThisMonth.map((category, i) => {
+            return <SpendPlanRow key={i} category={category}></SpendPlanRow>;
+          })}
+        </tbody>
+      </Table>
     </>
   );
 };
 
-export default SpendPlan;
+const mapStateToProps = (state) => ({
+  categoriesThisMonth: state.plaid.categoriesThisMonth,
+});
+
+// Note that there is probably a better way to do this with React hooks now
+export default connect(mapStateToProps, {})(SpendPlan);
