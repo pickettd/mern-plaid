adc633a9cba9c94bea083f115b172b9a6a5fd778
David Pickett <pickettd@gmail.com>
2020-07-24 15:04:48 -1000

    Setup redux store using toolkit but mvp version of non-toolkit reducers and actions etc

--- mlp-client/src/App.js
+++ mlp-client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import logo from "./logo.svg";
 import "./App.scss";
 import Header from "./components/layout/Header.js";
@@ -7,10 +9,15 @@ import Footer from "./components/layout/Footer.js";
 import SpendStory from "./components/spend/SpendStory.js";
 import SpendPlan from "./components/spend/SpendPlan.js";
 import ManageTransactions from "./components/spend/ManageTransactions.js";
+import rootReducer from "./reducers";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Router>
         <Header />
         <Switch>
@@ -38,7 +45,7 @@ function App() {
         </Switch>
         <Footer />
       </Router>
-    </>
+    </Provider>
   );
 }
 
