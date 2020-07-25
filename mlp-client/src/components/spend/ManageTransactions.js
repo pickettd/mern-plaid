12f775a5d91fb0d7f39143692b0e059d4bb29c70
David Pickett <pickettd@gmail.com>
2020-07-24 21:41:25 -1000

    Connect the manage transactions component to redux, setup bootstrap table with headers to show transaction rows, and map into plaid structure of banks and bank transactions

--- mlp-client/src/components/spend/ManageTransactions.js
+++ mlp-client/src/components/spend/ManageTransactions.js
@@ -1,11 +1,45 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
+import { connect } from "react-redux";
+import ManageTransactionRow from "./ManageTransactionRow";
 
-const ManageTransactions = () => {
+const ManageTransactions = (props) => {
   return (
     <>
       <div>This is the Manage Transactions Page</div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Bank Account</th>
+            <th>Transaction</th>
+            <th>Amount</th>
+            <th>Category</th>
+            <th>Update</th>
+            <th>Reviewed?</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.transactions.map((bank, i) => {
+            return bank.transactions.map((transaction, j) => {
+              return (
+                <ManageTransactionRow
+                  key={j}
+                  bankName={bank.accountName}
+                  transaction={transaction}
+                ></ManageTransactionRow>
+              );
+            });
+          })}
+        </tbody>
+      </Table>
     </>
   );
 };
 
-export default ManageTransactions;
+const mapStateToProps = (state) => ({
+  transactions: state.plaid.transactions,
+});
+
+// Note that there is probably a better way to do this with React hooks now
+export default connect(mapStateToProps, {})(ManageTransactions);
