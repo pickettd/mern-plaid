3c5b61ace2e2d30a8b6ef1e578f0ed4650535fe5
JoeBaranowski <joe@impactmaui.com>
2020-08-04 10:18:04 -1000

    Added Header HTML to Spend Plan
    yes that is what i did

--- mlp-client/src/components/spend/SpendPlan.js
+++ mlp-client/src/components/spend/SpendPlan.js
@@ -6,6 +6,61 @@ import SpendPlanRow from "./SpendPlanRow";
 const SpendPlan = (props) => {
   return (
     <>
+      <div className="section-space"></div>
+      <div className="section section-header-generic">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-6 col-sm-6">
+                    <div className="form-group">
+                      <select
+                        className="selectpicker"
+                        data-style="btn btn-default"
+                      >
+                        <option disabled selected>
+                          {" "}
+                          Choose date range
+                        </option>
+                        <option value="1">1 week </option>
+                        <option value="1">30 days</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <h1>
+                    Spend
+                    <br />
+                    <span className="small bottom">Plan</span>
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section section-header-generic">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h2>Income</h2>
+              <label for="pay-frequency">Pay Frequency</label>
+              <select className="selectpicker" data-style="btn btn-default">
+                <option disabled selected>
+                  {" "}
+                  Choose date range
+                </option>
+                <option value="1">Weekly</option>
+                <option value="1">Bi-weekly</option>
+                <option value="1">Bi-monthly</option>
+                <option value="1">Monthly</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
       <div>This is the Spend Plan</div>
       <Table striped bordered hover>
         <thead>
