6e90c09145a9676d4dff285f4a56c4c50c39ca93
David Pickett <pickettd@gmail.com>
2020-08-06 11:01:54 -1000

    Add files via upload

---
+++ precache-manifest.1f3079a8ad6c19e9a95f6d042efa3d92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca60332cd6ddb1b2b265057207a4f032",
+    "url": "/mern-plaid/index.html"
+  },
+  {
+    "revision": "0963515108349a137ea2",
+    "url": "/mern-plaid/static/css/main.d7b2e1ca.chunk.css"
+  },
+  {
+    "revision": "bb3f5093e7433933d22d",
+    "url": "/mern-plaid/static/js/2.a67d3f4b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mern-plaid/static/js/2.a67d3f4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0963515108349a137ea2",
+    "url": "/mern-plaid/static/js/main.b723b817.chunk.js"
+  },
+  {
+    "revision": "acf514c6fd0ca56eaf15",
+    "url": "/mern-plaid/static/js/runtime-main.8500bda8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mern-plaid/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9967d1aaeef841e9b9fccec0d11abcd.js"
+  "/mern-plaid/precache-manifest.1f3079a8ad6c19e9a95f6d042efa3d92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mern-plaid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
