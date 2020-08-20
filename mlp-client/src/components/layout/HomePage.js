64ec7eb19dadd97f8be1eeb7f80145cd46d90a7e
David Pickett <pickettd@gmail.com>
2020-08-20 11:20:27 -1000

    Add login and singup buttons to home screen

--- mlp-client/src/components/layout/HomePage.js
+++ mlp-client/src/components/layout/HomePage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import LoginButton from "../../components/profile/LoginButton.js";
+import SignupButton from "../../components/profile/SignupButton.js";
 
 const HomePage = (props) => {
   return (
     <div className="full-page mountain-background">
-      <div className="login-box  p-5">Login Box goes here</div>
+      <div className="login-box  p-5">
+        <LoginButton />
+        <br />
+        <SignupButton />
+      </div>
     </div>
   );
 };
