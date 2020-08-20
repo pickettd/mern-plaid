57533eb397eee7f252e18eb9f9cb6bc9f52d360a
David Pickett <pickettd@gmail.com>
2020-08-20 11:21:16 -1000

    Add redirection logic and private route logic (plus a sub component for the app content)

--- mlp-client/src/App.js
+++ mlp-client/src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
+import { useAuth0 } from "@auth0/auth0-react";
 import Auth0ProviderWithHistory from "./utils/auth0-provider-with-history.js";
+import PrivateRoute from "./utils/privateRoute.js";
+import Loading from "./utils/loading.js";
 import "./App.scss";
 import Header from "./components/layout/Header.js";
 import Footer from "./components/layout/Footer.js";
@@ -18,25 +26,40 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
+const Routing = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path={process.env.PUBLIC_URL + "/"}>
+          {isAuthenticated ? <Redirect to="/spend-story" /> : <HomePage />}
+        </Route>
+        <PrivateRoute path="/spend-story" component={SpendStory} />
+        <PrivateRoute path="/spend-plan" component={SpendPlan} />
+        <PrivateRoute
+          path="/manage-transactions"
+          component={ManageTransactions}
+        />
+        <PrivateRoute path="/user-profile" component={UserProfilePage} />
+        <PrivateRoute path="/bank-accounts" component={BankAccountsPage} />
+      </Switch>
+      <Footer />
+    </>
+  );
+};
+
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Auth0ProviderWithHistory>
-          <Header />
-          <Switch>
-            <Route
-              exact
-              path={process.env.PUBLIC_URL + "/"}
-              component={HomePage}
-            ></Route>
-            <Route path="/spend-story" component={SpendStory} />
-            <Route path="/spend-plan" component={SpendPlan} />
-            <Route path="/manage-transactions" component={ManageTransactions} />
-            <Route path="/user-profile" component={UserProfilePage} />
-            <Route path="/bank-accounts" component={BankAccountsPage} />
-          </Switch>
-          <Footer />
+          <Routing />
         </Auth0ProviderWithHistory>
       </Router>
     </Provider>
