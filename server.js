2db6381169ffb3314cb72fa5a7dbbc8bfa6e269d
David Pickett <pickettd@gmail.com>
2020-08-25 22:30:12 -1000

    Add correct path for react routing (forgot to add it in last commit)

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(express.static("mlp-client/build"));
 
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/mlp-client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
