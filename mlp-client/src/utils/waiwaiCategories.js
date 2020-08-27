9d286939c4c95681439049a19a83673b95e27c7b
David Pickett <pickettd@gmail.com>
2020-08-27 12:50:27 -1000

    Add utility file with structures of all the default waiwai categories

---
+++ mlp-client/src/utils/waiwaiCategories.js
@@ -0,0 +1,64 @@
+export const defaultCategoriesThisSpendRange = [
+  {
+    x: 1,
+    name: "Housing",
+  },
+  {
+    x: 2,
+    name: "Transportation",
+  },
+  {
+    x: 3,
+    name: "Food",
+  },
+  {
+    x: 4,
+    name: "Utilities",
+  },
+  {
+    x: 5,
+    name: "Insurance & Healthcare",
+  },
+  {
+    x: 6,
+    name: "Debt",
+  },
+  {
+    x: 7,
+    name: "Personal",
+  },
+  {
+    x: 8,
+    name: "Education",
+  },
+  {
+    x: 9,
+    name: "Child Care",
+  },
+  {
+    x: 10,
+    name: "Entertainment",
+  },
+  {
+    x: 11,
+    name: "Savings",
+  },
+  {
+    x: 12,
+    name: "Income",
+  },
+];
+export const defaultSpendingByCategory = {
+  Housing: 0,
+  Transportation: 0,
+  Food: 0,
+  Utilities: 0,
+  "Insurance & Healthcare": 0,
+  Debt: 0,
+  Personal: 0,
+  Education: 0,
+  "Child Care": 0,
+  Entertainment: 0,
+  Savings: 0,
+  Income: 0,
+};
