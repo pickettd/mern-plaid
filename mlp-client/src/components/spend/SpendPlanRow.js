fbb0eaf01583b9c3fcb8bdd8f9c7139244b12315
David Pickett <pickettd@gmail.com>
2020-08-31 14:02:15 -1000

    Update the spend plan row input to use the number type text input

--- mlp-client/src/components/spend/SpendPlanRow.js
+++ mlp-client/src/components/spend/SpendPlanRow.js
@@ -53,7 +53,7 @@ const SpendPlanRow = (props) => {
             Note that the currencyFormatter makes controlled edits difficult, e.g.
             value={currencyFormatter.format(budget)}
         */}
-        <input value={budget} onChange={onChangeValue}></input>
+        <input value={budget} type="number" onChange={onChangeValue}></input>
         <button className="btn secondary" onClick={() => saveButton()}>
           Save
         </button>
