07d2c08b57d8a6640d8b9e053990b972969cf109
David Pickett <pickettd@gmail.com>
2020-09-09 11:34:36 -1000

    Simplify the spend plan component to use regular spend plan row component for the income budget input and also use plan total row component to show the totals in both the expense and income sections. Which means a bunch of code related to getting/fetching data can be removed (and all the custom logic for dispatch/reducer to live-calculate the income totals

--- mlp-client/src/components/spend/SpendPlan.js
+++ mlp-client/src/components/spend/SpendPlan.js
@@ -1,153 +1,31 @@
-import React, { /*useState,*/ useReducer, useEffect, useCallback } from "react";
+import React from "react";
 import Table from "react-bootstrap/Table";
 import { connect } from "react-redux";
-import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "../../utils/loading.js";
-import SpendPlanRow from "./SpendPlanRow";
-import ColorHeader from "../layout/ColorHeader";
-import { currencyFormatter } from "../../utils/currencyFormatter";
-import { saveUserBudget } from "../../actions/authActions";
+import SpendPlanRow from "./SpendPlanRow.js";
+import ColorHeader from "../layout/ColorHeader.js";
+import PlanTotalRow from "./PlanTotalRow.js";
+// This component has the starter code for an income section with dropdowns
+//import UnusedIncomeBudget from "./UnusedIncomeBudget.js";
 
 // Note that currently we get a category from plaid called "Income"
 // Transactions that have just the category of "Income" are counted towards other income
 // In the manage transaction page, we show "Income" to the user as "Income - Other"
 // And we give the option to the user to recategorize as "Income - Paycheck"
 const payIncomeString = "Income - Paycheck";
+//const payIncomeDisplay = "Paycheck Total";
 const otherIncomeString = "Income";
+//const otherIncomeDisplay = "Other Income Total";
 
-const initialState = {
-  totalIncomeBudget: 0,
-  payIncomeBudget: 0,
-  otherIncomeBudget: 0,
-};
-
-// The reason we're using a reducer and not useState is because of the total money calculation
-// that relies on the state of the paycheck and other-income in order to setState
-const reducer = (state, action) => {
-  let processPayload = action.payload;
-  if (processPayload === "") {
-    processPayload = "0";
-  }
-  switch (action.type) {
-    case otherIncomeString:
-      return {
-        ...state,
-        otherIncomeBudget: action.payload,
-        totalIncomeBudget: state.payIncomeBudget + parseFloat(processPayload),
-      };
-    case payIncomeString:
-      return {
-        ...state,
-        payIncomeBudget: action.payload,
-        totalIncomeBudget: state.otherIncomeBudget + parseFloat(processPayload),
-      };
-    default:
-      return state;
-  }
-};
+// Putting this structure outside of the class so the spendrow props
+// don't change on budget save
+const incomeCategories = [
+  { name: payIncomeString },
+  { name: otherIncomeString },
+];
 
 const SpendPlan = (props) => {
-  const { getAccessTokenSilently } = useAuth0();
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const { budgets, saveUserBudget } = props;
-  const { payIncomeBudget, otherIncomeBudget } = state;
-
-  // The onBlur isn't working right now
-  //const [activePay, setActivePay] = useState(false);
-  //const [activeOther, setActiveOther] = useState(false);
-  //const mapSetActiveCall = {};
-  //mapSetActiveCall[otherIncomeString] = setActiveOther;
-  //mapSetActiveCall[payIncomeString] = setActivePay;
-
-  const setupReducer = useCallback(
-    (allBudgets) => {
-      if (allBudgets) {
-        if (allBudgets[otherIncomeString]) {
-          dispatch({
-            type: otherIncomeString,
-            payload: allBudgets[otherIncomeString],
-          });
-        }
-        if (allBudgets[payIncomeString]) {
-          dispatch({
-            type: payIncomeString,
-            payload: allBudgets[payIncomeString],
-          });
-        }
-      }
-    },
-    [dispatch]
-  );
-
-  useEffect(() => {
-    setupReducer(budgets);
-  }, [budgets, setupReducer]);
-
-  // This onblur tries to reset the row value and hide the button.
-  // Problem is that the button hides before the onClick fires/happens
-  // And that problem happens if it is using dispatch or even just setActive(false)
-  /*
-  const onIncomeBlur = (event, categoryName) => {
-    let dispatchValue = "";
-    if (budgets && budgets[categoryName]) {
-      dispatchValue = budgets[categoryName];
-    }
-    dispatch({
-      type: categoryName,
-      payload: dispatchValue,
-    });
-    mapSetActiveCall[categoryName](false);
-  };*/
-
-  const onIncomeSave = useCallback(
-    (categoryName) => {
-      const budgetData = { name: categoryName, payload: {} };
-      let budget = "";
-      if (categoryName === payIncomeString) {
-        budget = payIncomeBudget;
-      } else {
-        budget = otherIncomeBudget;
-      }
-      const trimmedBudget = budget.trim();
-      // This checks if the string in budget is a number
-      const valid = trimmedBudget !== "" && !isNaN(trimmedBudget);
-
-      if (valid) {
-        budgetData.payload[categoryName] = parseFloat(trimmedBudget);
-        // We only save the budget if it is a number
-        // But should have UI here to tell the user something wrong happened if it isn't a number
-        getAccessTokenSilently().then((accessToken) => {
-          saveUserBudget(accessToken, budgetData);
-        });
-        // The onBlur isn't working right now
-        //mapSetActiveCall[categoryName](false);
-      }
-    },
-    [
-      payIncomeBudget,
-      otherIncomeBudget,
-      getAccessTokenSilently,
-      saveUserBudget,
-      // The onBlur isn't working right now
-      //mapSetActiveCall,
-    ]
-  );
-
-  const onIncomeBudgetChange = (event, categoryName) => {
-    let justNumber = event.target.value;
-    // Note this operation shouldn't be necessary anymore
-    // (at least in Chrome, can't type $ into numeric input)
-    if (justNumber.charAt(0) === "$") {
-      justNumber = justNumber.substring(1);
-    }
-    const newBudgetAmount = justNumber.trim();
-
-    // We handle the case of an empty string in the reducer
-    dispatch({ type: categoryName, payload: newBudgetAmount });
-    // The onBlur isn't working right now
-    //mapSetActiveCall[categoryName](true);
-  };
+  const { budgets } = props;
 
   if (props.accountsLoading || props.transactionsLoading) {
     return <Loading />;
@@ -169,128 +47,23 @@ const SpendPlan = (props) => {
                   <tr>
                     <th>Waiwai category</th>
                     <th></th>
-                    <th>Budget for {props.spendRangeDays} days</th>
+                    <th>Income budget for {props.spendRangeDays} days</th>
                     <th>Earned last {props.spendRangeDays} days</th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Income Total</td>
-                    <td></td>
-                    <td>{currencyFormatter.format(state.totalIncomeBudget)}</td>
-                    <td>{currencyFormatter.format(props.incomeSum)}</td>
-                  </tr>
-                  <tr>
-                    <td>Paycheck Total</td>
-                    <td></td>
-                    <td>{currencyFormatter.format(state.payIncomeBudget)}</td>
-                    <td>{currencyFormatter.format(props.paycheckSum)}</td>
-                  </tr>
-                  <tr>
-                    <td>Other Income Total</td>
-                    <td></td>
-                    <td>{currencyFormatter.format(state.otherIncomeBudget)}</td>
-                    <td>{currencyFormatter.format(props.otherIncomeSum)}</td>
-                  </tr>
+                  <PlanTotalRow rowDisplayName="Income" />
+                  {incomeCategories.map((category) => {
+                    return (
+                      <SpendPlanRow
+                        key={category.name}
+                        category={category}
+                        propBudget={budgets[category.name]}
+                      ></SpendPlanRow>
+                    );
+                  })}
                 </tbody>
               </Table>
-              <div className="mt-5">
-                <h3>Add Income Sources</h3>
-              </div>
-              <div className="row">
-                <div className="col">
-                  {/*<label htmlFor="pay-frequency">Paycheck Frequency</label>
-                  <br />
-                  <select
-                    className="selectpicker form-control custom-select"
-                    data-style="btn btn-default"
-                    defaultValue="0"
-                  >
-                    <option disabled value="0">
-                      {" "}
-                      Choose date range
-                    </option>
-                    <option value="1">Weekly</option>
-                    <option value="1">Bi-weekly</option>
-                    <option value="1">Bi-monthly</option>
-                    <option value="1">Monthly</option>
-                  </select>
-                  <br />
-                  <label className="mt-4">Paycheck amount</label>*/}
-                  <label className="mt-4">
-                    Expected Paycheck Total For {props.spendRangeDays} Days
-                  </label>
-                  <br />
-                  <input
-                    value={state.payIncomeBudget}
-                    onChange={(event) =>
-                      onIncomeBudgetChange(event, payIncomeString)
-                    }
-                    type="number"
-                    className="form-control"
-                    // The onBlur isn't working right now
-                    //onBlur={(event) => onIncomeBlur(event, payIncomeString)}
-                  ></input>
-                </div>
-                <div className="col">
-                  {/*<label htmlFor="other-frequency">
-                    Other Income Frequency
-                  </label>
-                  <br />
-                  <select
-                    className="selectpicker form-control custom-select"
-                    data-style="btn btn-default"
-                    defaultValue="0"
-                  >
-                    <option disabled value="0">
-                      {" "}
-                      Choose date range
-                    </option>
-                    <option value="1">Weekly</option>
-                    <option value="1">Bi-weekly</option>
-                    <option value="1">Bi-monthly</option>
-                    <option value="1">Monthly</option>
-                  </select>
-                  <br />
-                  <label className="mt-4">Other Income Amount</label>*/}
-                  <label className="mt-4">
-                    Expected Other Income Total For {props.spendRangeDays} Days
-                  </label>
-                  <br />
-                  <input
-                    value={state.otherIncomeBudget}
-                    onChange={(event) =>
-                      onIncomeBudgetChange(event, otherIncomeString)
-                    }
-                    type="number"
-                    className="form-control"
-                    // The onBlur isn't working right now
-                    //onBlur={(event) => onIncomeBlur(event, otherIncomeString)}
-                  ></input>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <button
-                    onClick={() => onIncomeSave(payIncomeString)}
-                    className="btn secondary mt-4"
-                    // This is designed to work with the onBlur that isn't working right now
-                    //style={{ visibility: activePay ? "visible" : "hidden" }}
-                  >
-                    Save
-                  </button>
-                </div>
-                <div className="col">
-                  <button
-                    onClick={() => onIncomeSave(otherIncomeString)}
-                    className="btn secondary mt-4"
-                    // This is designed to work with the onBlur that isn't working right now
-                    //style={{ visibility: activeOther ? "visible" : "hidden" }}
-                  >
-                    Save
-                  </button>
-                </div>
-              </div>
             </div>
           </div>
         </div>
@@ -305,11 +78,12 @@ const SpendPlan = (props) => {
                   <tr>
                     <th>Waiwai category</th>
                     <th></th>
-                    <th>Budget for {props.spendRangeDays} days</th>
+                    <th>Expense budget for {props.spendRangeDays} days</th>
                     <th>Spent last {props.spendRangeDays} days</th>
                   </tr>
                 </thead>
                 <tbody>
+                  <PlanTotalRow rowDisplayName="Expense" />
                   {props.categoriesThisSpendRange.map((category, i) => {
                     if (category.name.includes("Income")) {
                       return (
@@ -344,7 +118,7 @@ const mapStateToProps = (state) => ({
   accountsLoading: state.plaid.accountsLoading,
   transactionsLoading: state.plaid.transactionsLoading,
 });
-const mapDispatchToProps = { saveUserBudget };
+const mapDispatchToProps = {};
 
 // Note that there is probably a better way to do this with React hooks now
 export default connect(mapStateToProps, mapDispatchToProps)(SpendPlan);
