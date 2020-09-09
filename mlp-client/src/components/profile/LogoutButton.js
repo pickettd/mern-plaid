f62319f5e4ea1a9214319ce79a66a0897cb4b004
David Pickett <pickettd@gmail.com>
2020-09-08 14:31:29 -1000

    No need to use bootstrap button component for displaying log out since we want it to look like a link

--- mlp-client/src/components/profile/LogoutButton.js
+++ mlp-client/src/components/profile/LogoutButton.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "react-bootstrap";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
-    <Button
+    <button
       onClick={() =>
         logout({
           returnTo: window.location.origin + process.env.PUBLIC_URL,
         })
       }
-      variant="danger"
-      className="btn-margin"
     >
       Log Out
-    </Button>
+    </button>
   );
 };
 
